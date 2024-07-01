1b30af9d5e3aa10ce2f729676d9bcfd7e5de963e
GautamGirdhar <girdhargautam6@gmail.com>
2024-07-01 15:46:14 +0530

    first commit

---
+++ data/database.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb');
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function connect() {
+    const client  = await MongoClient.connect('mongodb+srv://girdhargautam6:G%40ut%40m%4010122004@cluster0.i41grdl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    database = client.db('blog');
+}
+
+function getDb() {
+    if (!database) {
+        throw {message: 'Database connection not established!'};
+    }
+
+    return database;
+}
+
+module.exports = {
+    connectToDatabase: connect,
+    getDb: getDb
+};
+
